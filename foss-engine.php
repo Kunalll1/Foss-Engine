4098439603103fe23dc8bcb924366349de8c42ad
Kunalll1 <kunalkumarjha97@gmail.com>
2025-04-15 15:28:38 +0530

    fix the typo

--- foss-engine.php
+++ foss-engine.php
@@ -55,7 +55,7 @@ register_deactivation_hook(__FILE__, 'deactivate_wp_content_generator');
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path(__FILE__) . 'includes/class-foss-engion.php';
+require plugin_dir_path(__FILE__) . 'includes/class-foss-engine.php';
 
 /**
  * Begins execution of the plugin.
